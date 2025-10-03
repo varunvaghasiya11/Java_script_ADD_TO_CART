c6fd279edfed9c6d3699f5322379f1aa81e170f1
Varun Vaghasiya <varunvaghasiya@192.168.1.77>
2025-10-03 15:22:56 +0530

    first commit

---
+++ cart.js
@@ -0,0 +1,74 @@
+function cartproduct() {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    let Total = 0;
+
+    let print = document.querySelector('#row');
+    let str = '';
+
+    if (cart.length === 0) {
+        str = `<div class="text-center text-muted my-5 col-12">
+                  <h3 class="m-0">Your cart is empty</h3>
+               </div>`;
+    } else {
+        cart.forEach(item => {
+            Total += item.price * item.qty
+            str += `
+            <tr class="text-center align-middle">
+                        <td class="">
+                            <img src="${item.thumbnail}" width="100" alt="Product Image">
+                        </td>
+                        <td class="">
+                            <h3 class="card-title text-black">${item.title}</h3>
+                        </td>
+                        <td>                        
+                        <button class="btn badge bg-dark text-white" onclick="return decqty(${item.id})">-</button>
+                        <span class="badge bg-dark text-white">${item.qty}</span>
+                        <button class="btn badge bg-dark text-white" onclick="return addqty(${item.id})">+</button>
+                        </td>
+                        <td>
+                        <p class="m-0">₹${item.price * item.qty.toFixed(2)}</p>
+                        </td>
+                        <td>
+                        <button class="btn btn-danger btn-sm" onclick="removeItem(${item.id})">Remove</button>
+                        </td>
+                    </tr>`;
+        });
+    }
+    print.innerHTML = str;
+    if(cart.length == 0){
+        document.getElementById('carditemtotal').style.display = "none";
+        document.getElementById('header').style.display = "none";
+    }else{
+        document.getElementById('carditemtotal').innerHTML = `Total : ₹${Total.toFixed(2)}`
+    }
+}
+
+function addqty(id) {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    cart.forEach(ele => {
+        if (ele.id == id) {
+            ele.qty = ele.qty + 1;
+        }
+    })
+    localStorage.setItem('cart', JSON.stringify(cart));
+    cartproduct();
+}
+function decqty(id) {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    cart.forEach(ele => {
+        if (ele.id == id && ele.qty > 1) {
+            ele.qty = ele.qty - 1;
+        }
+    })
+    localStorage.setItem('cart', JSON.stringify(cart));
+    cartproduct();
+}
+
+function removeItem(id) {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    cart = cart.filter(item => item.id !== id);
+    localStorage.setItem('cart', JSON.stringify(cart));
+    cartproduct();
+}
+
+cartproduct();
